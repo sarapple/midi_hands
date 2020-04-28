52634b09b156d05d1ec029ba4785bdabb526aa9b
sarapple <1sarabyte@gmail.com>
2020-04-28 15:28:42 -0700

    cleand up comments

--- lstm_audio.py
+++ lstm_audio.py
@@ -21,14 +21,9 @@ class LSTMAudio(torch.nn.Module):
         self.num_layers = num_layers
         self.dropout = dropout
 
-        # self.model_layers_callbacks = []
-        # self.lstm_1 = torch.nn.LSTM(input_dim, hidden_dim, 1, batch_first=True)
-        # self.lstm_2 = torch.nn.LSTM(hidden_dim, hidden_dim, 1, batch_first=True)
-        # self.lstm_3 = torch.nn.LSTM(hidden_dim, hidden_dim, 1, batch_first=True)
-        # self.relu = torch.nn.LeakyReLU()
-
         model_dict = {}
         self.model_layers = model_layers
+        # Making the layers into a dictionary is required to use torch.nn.ModuleDict
         for layer in list(set(model_layers)):
             if (layer == "lstm_start"):
                 model_dict[layer] = torch.nn.LSTM(input_dim, hidden_dim, 1, batch_first=True)
@@ -47,8 +42,8 @@ class LSTMAudio(torch.nn.Module):
 
         self.model = torch.nn.ModuleDict(model_dict)
         self.model_layers_callbacks = []
-            # model_dict["lstm"]
-        # Generate the inputs and output calls for each layerr
+
+        # Generate the inputs and output calls for each layer. The layer names are determined from trains.json
         for index, layer in enumerate(model_layers):
             if (layer == "lstm" or layer == "lstm_start"):
                 if (layer == "lstm_start"):
@@ -84,24 +79,12 @@ class LSTMAudio(torch.nn.Module):
             else:
                 raise Exception("Unhandled layer in LSTM model.")
 
-        
-        # # Define the output layer
-        # # self.linear = torch.nn.Linear(self.hidden_dim, output_dim)
-        # self.model_layers = torch.nn.Sequential(**self.model_layers, torch.nn.Linear(self.hidden_dim, output_dim))
-
-    def forward(self, input_data): # comes in batch_size x 16 x 6
-        # import ipdb; ipdb.set_trace();
+    def forward(self, input_data):
+        # Use layers defined in trains.json (optinos: dropout, relu, linear, lstm, batch_norm)
         x = input_data
         for layer_instance_call, layer in self.model_layers_callbacks:
             x = layer_instance_call(x, self.model[layer])
 
-        # x, _ = self.model_layers(x)
-        # x, _ = self.lstm_2(x)
-        # x, _ = self.lstm_3(x)
-        # x = self.relu(x)
-
-        # y_pred = self.linear(x) # batch_size (batch size) x 16 (sequence) x 1 (output dim)
-        
         return x
     
     def get_accuracy(self, outputs, truths):
@@ -109,7 +92,6 @@ class LSTMAudio(torch.nn.Module):
         outputs_flattened = outputs.squeeze()
         total_counter = 0
         correct_counter = 0
-        # import ipdb; ipdb.set_trace();
         for batch_index, sequence in enumerate(outputs_flattened):
             for note_index, note_prediction in enumerate(sequence):
                 total_counter += 1
